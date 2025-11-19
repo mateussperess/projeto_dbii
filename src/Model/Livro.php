cde7964443bcacb165781e945debe78acece3047
Mateus Peres <mateuspereslopesl@outlook.com>
2025-11-19 11:45:22 -0300

    feat: livro's getters and setters

--- src/Model/Livro.php
+++ src/Model/Livro.php
@@ -43,4 +43,96 @@ class Livro implements JsonSerializable
   {
     throw new \Exception('Not implemented');
   }
+
+  // Getters
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function getTitulo()
+  {
+    return $this->titulo;
+  }
+
+  public function getAutor()
+  {
+    return $this->autor;
+  }
+
+  public function getDescricao()
+  {
+    return $this->descricao;
+  }
+
+  public function getAno()
+  {
+    return $this->ano;
+  }
+
+  public function getNumeroPaginas()
+  {
+    return $this->numeroPaginas;
+  }
+
+  public function getIsLocated()
+  {
+    return $this->isLocated;
+  }
+
+  public function getNumeroLocacoes()
+  {
+    return $this->numeroLocacoes;
+  }
+
+  public function getIdGenero()
+  {
+    return $this->idGenero;
+  }
+
+  // Setters
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  public function setTitulo($titulo)
+  {
+    $this->titulo = $titulo;
+  }
+
+  public function setAutor($autor)
+  {
+    $this->autor = $autor;
+  }
+
+  public function setDescricao($descricao)
+  {
+    $this->descricao = $descricao;
+  }
+
+  public function setAno($ano)
+  {
+    $this->ano = $ano;
+  }
+
+  public function setNumeroPaginas($numeroPaginas)
+  {
+    $this->numeroPaginas = $numeroPaginas;
+  }
+
+  public function setIsLocated($isLocated)
+  {
+    $this->isLocated = $isLocated;
+  }
+
+  public function setNumeroLocacoes($numeroLocacoes)
+  {
+    $this->numeroLocacoes = $numeroLocacoes;
+  }
+
+  public function setIdGenero($idGenero)
+  {
+    $this->idGenero = $idGenero;
+  }
 }
